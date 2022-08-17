ddc8dea1504dff0af2a37e414bf3e08653a999ab
rakhimovsh <shukurulloh.com@gmail.com>
2022-08-17 16:10:58 +0500

    configurate redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,26 @@
 import {Suspense, StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from "react-router-dom";
+import {Provider} from 'react-redux'
 
 import App from './App';
 import Loader from "./components/Loader";
+import {setupStore} from "./redux/store";
+
+const store = setupStore()
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <StrictMode>
-        <BrowserRouter>
-            <Suspense fallback={<Loader />}>
-                <App />
-            </Suspense>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+                <Suspense fallback={<Loader />}>
+                    <App />
+                </Suspense>
+            </BrowserRouter>
+        </Provider>
     </StrictMode>
 );
 
